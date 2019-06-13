31e4951572b5373b76edca0411ed479dee66b04e
nnnkit <iankit17@gmail.com>
2019-06-13 20:26:03 +0530

    add function types exercise

---
+++ assignments/functionTypes.js
@@ -0,0 +1,159 @@
+// Write the below function in five different forms. i.e
+// 1. Function Decleration
+// 2. Function Expression
+// 3. Arrow Function Without Curly Braces
+// 4. Arrow Function With Curly Braces
+// 5. Function Invocation
+
+// Example Start
+/**
+ * Converts a number a string.
+ * @param {number} n
+ * @return {string} the number as a string
+ */
+
+//Function Decleration
+function convertToString(n) {
+  return String(n);
+}
+
+// Function Expression
+let convertToString1 = function(n) {
+  return String(n);
+};
+
+// Arrow Function
+let convertToString = n => String(n);
+
+// Arrow Function With Curly Bracket
+
+let convertToString = n => {
+  return String(n);
+};
+
+// Function Invocation
+
+convertToString(21); // "21"
+
+// Example End
+
+/**
+ * Adds one to a given number.
+ * @param {number} n
+ * @return {number}
+ */
+
+/**
+ * Subtracts one from a given number.
+ * @param {number} n
+ * @return {number}
+ */
+
+/**
+ * Adds two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the sum
+ */
+
+/**
+ * Subtracts the second number from the first.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the difference
+ */
+
+/**
+ * Multiplies two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the product
+ */
+
+/**
+ * Divides the first number by the second.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the quotient
+ */
+
+/**
+ * Multiplies a number by itself.
+ * @param {number} x, number to be squared
+ * @return {number} squared
+ */
+
+/**
+ * Performs a mathematical operation on two numbers.
+ * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
+ * @param {string} operation "add", "subtract", "multiply", or "divide"
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the result
+ */
+
+/**
+ * Returns true if `a` is greater than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is larger than `b`
+ */
+
+/**
+ * Returns true if `a` is less than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is smaller than `b`
+ */
+
+/**
+ * Returns true if `a` and `b` are equal.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} the numbers are equal
+ */
+
+/**
+ * Returns the smallest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the smallest number
+ */
+
+/**
+ * Returns the largest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the largest number
+ */
+
+/**
+ * Returns true if `n` is even.
+ * @param {number} n
+ * @return {boolean} the number is even
+ */
+
+/**
+ * Returns true if `n` is odd.
+ * @param {number} n
+ * @return {boolean} the number is odd
+ */
+
+/**
+ * Returns a letter grade.
+ * "A": 90-100%
+ * "B": 80-89%
+ * "C": 70-79%
+ * "D": 60-69%
+ * "F": 0-59%
+ * @param {number} score
+ * @param {number} total maximum possible score
+ * @return {string} the score represented as a letter grade
+ */
+
+/**
+ * Joins two strings with a space.
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string} joined the words joined with a space
+ */
